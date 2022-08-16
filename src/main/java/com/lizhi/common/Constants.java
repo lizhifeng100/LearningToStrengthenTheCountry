96c4411843ee212d6707e8c36179e30364b6239e
lizhi <2933479745@qq.com>
2022-08-16 22:44:38 +0800

    Initial Commit

---
+++ src/main/java/com/lizhi/common/Constants.java
@@ -0,0 +1,27 @@
+package com.lizhi.common;
+
+/**
+ * @author lizhi
+ */
+public class Constants {
+
+    public Constants() {
+    }
+
+    public static String HOME_PAGE = "https://www.xuexi.cn/";
+
+    public static String LOGIN_LINK = "https://pc.xuexi.cn/points/login.html";
+
+    public static String ARTICLES_LINK = "https://www.xuexi.cn/d05cad69216e688d304bb91ef3aac4c6/9a3668c13f6e303932b5e0e100fc248b.html";
+
+    public static String SCORES_LINK = "https://pc.xuexi.cn/points/my-points.html";
+
+    public static String VIDEO_LINK = "https://www.xuexi.cn/a191dbc3067d516c3e2e17e2e08953d6/b87d700beee2c44826a9202c75d18c85.html?pageNumber=39";
+
+    public static String DAILY_EXAM_PRACTICE = "https://pc.xuexi.cn/points/exam-practice.html";
+
+    public static String SPECIAL_QUESTIONS = "https://pc.xuexi.cn/points/exam-paper-list.html";
+
+    public static String WEEKLY_QUESTIONS = "https://pc.xuexi.cn/points/exam-weekly-list.html";
+
+}
